1e8c360e12a9a30832bc8ee3147a671006140690
Matt Campbell <mecampbell25@gmail.com>
2014-11-09 16:37:01 -0500

    Working awesome version!

--- app/helpers/application_helper.rb
+++ app/helpers/application_helper.rb
@@ -1,2 +1,5 @@
 module ApplicationHelper
+  def controller_name_and_action
+    "#{params[:controller]}-#{params[:action]}"
+  end
 end
--- app/models/user.rb
+++ app/models/user.rb
@@ -1,7 +1,10 @@
 class User < ActiveRecord::Base
+  has_secure_password
+
   has_many :posts
   has_many :comments
 
   validates_uniqueness_of :name
   validates_presence_of :name
+  validates_presence_of :password
 end
