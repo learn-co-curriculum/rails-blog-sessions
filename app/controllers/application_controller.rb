63761fca350417a5b5c8d791c4b622921afb24d9
joshuabamboo <joshuaowens011@gmail.com>
2015-10-07 18:16:04 -0400

    tests pass

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -3,4 +3,17 @@ class ApplicationController < ActionController::Base
   # For APIs, you may want to use :null_session instead.
   protect_from_forgery with: :exception
 
+  helper_method :current_user, :user_signed_in?
+
+  def current_user
+    @current_user ||= session[:user_id] && User.find_by_id(session[:user_id])
+  end
+
+  def user_signed_in?
+    session[:user_id] ? true:false
+  end
+
+  def authorize
+    !user_signed_in?
+  end
 end
--- app/controllers/users_controller.rb
+++ app/controllers/users_controller.rb
@@ -15,7 +15,9 @@ class UsersController < ApplicationController
   def create
     @user = User.new(user_params)
     if @user.save
-      redirect_to user_path(@user), :notice => "Thank you for signing up!"
+      # redirect_to user_path(@user), :notice => "Thank you for signing up!"
+      session[:user_id] = @user.id
+      redirect_to root_path, notice: "Thank you for signing up, #{@user.name}!"
     else
       render :new
     end
@@ -27,6 +29,6 @@ class UsersController < ApplicationController
     end
 
     def user_params
-      params.require(:user).permit(:name)
+      params.require(:user).permit(:name, :password, :password_confirmation, :email)
     end
 end
